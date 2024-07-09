1511186ab3e2cb393b7873b9257caf7ba617a495
wlangera <ward.langeraert@inbo.be>
2024-07-09 14:18:20 +0200

    initial commit

---
+++ R/map_simulate_occurrences.R
@@ -0,0 +1,25 @@
+#' Simulate occurrences within a spatiotemporal scope
+#'
+#' The function simulates occurrences of a species within a given spatial
+#' and/or temporal extend.
+#'
+#' @param df ...
+#' @param ... ...
+#'
+#' @returns ...
+#'
+#' @export ...
+#'
+#' @import dplyr
+#' @import purrr
+#'
+#' @family multispecies
+#'
+#' @examples
+#'
+
+map_simulate_occurrences <- function(
+    df,
+    ...) {
+
+}
