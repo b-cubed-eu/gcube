7cec77d44a14b7fc7bf223b4c731aafbf735d52b
wlangera <ward.langeraert@inbo.be>
2024-07-09 14:31:09 +0200

    function skeleton

--- R/map_simulate_occurrences.R
+++ R/map_simulate_occurrences.R
@@ -12,6 +12,8 @@
 #'
 #' @import dplyr
 #' @import purrr
+#' @import tidyr
+#' @import assertthat
 #'
 #' @family multispecies
 #'
@@ -20,6 +22,35 @@
 
 map_simulate_occurrences <- function(
     df,
-    ...) {
+    ...,
+    unnest = TRUE) {
+  ### Start checks
+  # 1. Check input type and length
+  # Check if df is a dataframe
+  stopifnot("`df` must be a dataframe." = inherits(df, "data.frame"))
 
+  # Check if aggregate is a logical vector of length 1
+  stopifnot("`unnest` must be a logical vector of length 1." =
+              assertthat::is.flag(unnest) && assertthat::noNA(unnest))
+
+
+  out_df <- df %>%
+    mutate(
+      occurrences = purrr::pmap(
+        dplyr::select(.,
+                      "plgn",
+                      "initial_average_abundance",
+                      "n_time_points",
+                      "temporal_function",
+                      "sd_step",
+                      "spatial_autocorr",
+                      "seed"),
+        simulate_occurrences))
+
+  if (unnest) {
+    out_df <- out_df %>%
+      tidyr::unnest(cols = "occurrences")
+  }
+
+  return(out_df)
 }
