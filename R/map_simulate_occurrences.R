46929f4036de9acea8679b98e5365e6fcadbefdb
wlangera <ward.langeraert@inbo.be>
2024-07-09 15:55:11 +0200

    finalise raw function

--- R/map_simulate_occurrences.R
+++ R/map_simulate_occurrences.R
@@ -4,7 +4,8 @@
 #' and/or temporal extend.
 #'
 #' @param df ...
-#' @param ... ...
+#' @param unnest Logical. If `TRUE` (default), ... Otherwise ...
+#' @param arg_list ...
 #'
 #' @returns ...
 #'
@@ -22,8 +23,8 @@
 
 map_simulate_occurrences <- function(
     df,
-    ...,
-    unnest = TRUE) {
+    unnest = TRUE,
+    arg_list = NA) {
   ### Start checks
   # 1. Check input type and length
   # Check if df is a dataframe
@@ -33,19 +34,31 @@ map_simulate_occurrences <- function(
   stopifnot("`unnest` must be a logical vector of length 1." =
               assertthat::is.flag(unnest) && assertthat::noNA(unnest))
 
+  ### End checks
+
+  sim_occ_args <- formalArgs(simulate_occurrences)
+  sim_occ_names <- sim_occ_args[sim_occ_args != "..."]
+
+  temp_f_args <- sapply(df$temporal_function, function(f) {
+    if (is.function(f)) formalArgs(f)
+  })
+  temp_f_names <- unique(unlist(temp_f_args))
+
+  col_arg_names <- unique(c(sim_occ_names, temp_f_names))
+
+  if (!is.na(arg_list)) {
+    df <- df %>%
+      rename(!!!arg_list)
+  }
+
+  selection_names <- intersect(names(df), col_arg_names)
 
   out_df <- df %>%
     mutate(
       occurrences = purrr::pmap(
-        dplyr::select(.,
-                      "plgn",
-                      "initial_average_abundance",
-                      "n_time_points",
-                      "temporal_function",
-                      "sd_step",
-                      "spatial_autocorr",
-                      "seed"),
-        simulate_occurrences))
+        dplyr::select(., all_of(selection_names)),
+        simulate_occurrences)
+    )
 
   if (unnest) {
     out_df <- out_df %>%
