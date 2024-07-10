488b6d004bf2dab51f0d0dbee71052c2598d9a5a
wlangera <ward.langeraert@inbo.be>
2024-07-10 14:54:29 +0200

    set example data unit tests

--- tests/testthat/test-map_simulate_occurrences.R
+++ tests/testthat/test-map_simulate_occurrences.R
@@ -1,7 +1,35 @@
-# ellipsis?
-# keep original column names
-# nest = false same result if you nest afterwards
+## Prepare example datasets
+# Create polygon
+plgn <- st_polygon(list(cbind(c(5, 10, 8, 2, 3, 5), c(2, 1, 7, 9, 5, 2))))
+
+# Specify dataframe for 3 species with custom function arguments
+# Dataframe with column names equal to arguments
+species_dataset_df1 <- tibble(
+  taxonID = c("species1", "species2", "species3"),
+  plgn = rep(list(plgn), 3),
+  initial_average_abundance = c(50, 100, 500),
+  n_time_points = rep(6, 3),
+  temporal_function = c(simulate_random_walk, simulate_random_walk, NA),
+  sd_step = c(1, 1, NA),
+  spatial_autocorr = "random",
+  seed = 123)
+
+# Dataframe with custom column names and named list for argument conversion
+species_dataset_df2 <- species_dataset_df1 %>%
+  rename(polygon = plgn,
+         sd = sd_step)
+
+arg_conv_list2 <- list(
+    plgn = "polygon",
+    sd_step = "sd"
+  )
+
+## Unit tests
 
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+# Do with and without named list and should be the same
+# keep original column names
+# nest = false same result if you nest afterwards
