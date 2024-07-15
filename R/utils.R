97f0f187c24e8233b6f19b70571ff552c79e240e
wlangera <ward.langeraert@inbo.be>
2024-07-15 20:58:18 +0200

    handle mapped warnings

--- R/utils.R
+++ R/utils.R
@@ -31,3 +31,45 @@ get_function_arguments <- function(f, df) {
 
   return(f_args)
 }
+
+
+#' Function to return repeated warnings during mapping
+#'
+#' This function stores all warnings captured by `purrr::quietly()` that
+#' occurred during mapping and reports them in a clean way.
+#'
+#' @param df A dataframe containing a list-column with the output of a mapped
+#' function wrapped with `purrr::quietly()`.
+#' @param mapped_col The name of the list-column described above. Defaults to
+#' `"mapped_col"`.
+#'
+#' @noRd
+
+handle_mapped_warnings <- function(df, mapped_col = "mapped_col") {
+  ### Start checks
+  stopifnot("`mapped_col` not present in provided dataframe." =
+              mapped_col %in% colnames(df))
+  stopifnot("`mapped_col` must be a list-column." =
+              inherits(df[, mapped_col], "list"))
+  ### End checks
+
+  # Collect and count warnings in dataframe
+  warning_df <- data.frame(
+    table(unlist(sapply(df[, mapped_col], function(warn) warn$warnings)))
+  )
+
+  # Print warnings
+  if (nrow(warning_df) > 0) {
+    message(paste(sum(warning_df$Freq), "warnings during mapping:"))
+    for (i in seq_len(nrow(warning_df))) {
+      message <- warning_df[i, "Var1"]
+      count <- warning_df[i, "Freq"]
+      warning(paste0(message, " [", count, " times]"))
+    }
+  }
+
+  # Only retain result from mapping
+  df$mapped_col = sapply(df[, mapped_col], function(val) val$result)
+
+  return(df)
+}
