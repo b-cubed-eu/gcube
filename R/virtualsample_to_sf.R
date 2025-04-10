c11a8a706520599f422be57a207b92ae78cc21fc
Ward Langeraert <ward.langeraert@inbo.be>
2025-04-10 17:19:35 +0200

    add option to extract from raster

--- R/virtualsample_to_sf.R
+++ R/virtualsample_to_sf.R
@@ -1,4 +1,4 @@
-virtualsample_to_sf <- function(virtual_sample) {
+virtualsample_to_sf <- function(virtual_sample, raster_lyr = NULL) {
   # Virtual samples to sf object
   sample_sf <- sf::st_as_sf(
       virtual_sample$sample.points,
@@ -6,12 +6,22 @@ virtualsample_to_sf <- function(virtual_sample) {
       crs = terra::crs(virtual_sample$original.distribution.raster)
     )
 
+  # Extract raster values if provided
+  if (!is.null(raster_lyr)) {
+    extracted_values <- terra::extract(raster_lyr, sample_sf, ID = FALSE)
+    sample_sf <- cbind(sample_sf, extracted_values)
+  }
+
   # Add information to dataset and select output columns
-  det_prob <- virtual_sample$detection.probability$detection.probability
-  sample_sf %>%
+  out_df <- sample_sf %>%
     dplyr::mutate(
       id = rownames(virtual_sample$sample.points),
-      detection_probability = det_prob,
-      observed = !is.na(.data$Observed)) %>%
-    dplyr::select("id", "observed", "geometry")
+      observed = !is.na(.data$Observed)
+    ) %>%
+    dplyr::select(-c("Real", "Observed")) %>%
+    dplyr::select("id", "observed", dplyr::everything(), "geometry")
+
+  rownames(out_df) <- NULL
+
+  return(out_df)
 }
