b82263f51592e0ea91686a5329181627a6925e1b
Ward Langeraert <ward.langeraert@inbo.be>
2025-04-10 16:27:11 +0200

    add detection prob

--- R/virtualsample_to_sf.R
+++ R/virtualsample_to_sf.R
@@ -1,10 +1,17 @@
 virtualsample_to_sf <- function(virtual_sample) {
-  detections_df_raw <- sf::st_as_sf(
+  # Virtual samples to sf object
+  sample_sf <- sf::st_as_sf(
       virtual_sample$sample.points,
       coords = c("x", "y"),
       crs = terra::crs(virtual_sample$original.distribution.raster)
-    ) %>%
-    dplyr::mutate(observed = !is.na(.data$Observed))
+    )
 
-
+  # Add information to dataset and select output columns
+  det_prob <- virtual_sample$detection.probability$detection.probability
+  sample_sf %>%
+    dplyr::mutate(
+      id = rownames(virtual_sample$sample.points),
+      detection_probability = det_prob,
+      observed = !is.na(.data$Observed)) %>%
+    dplyr::select("id", "observed", "geometry")
 }
